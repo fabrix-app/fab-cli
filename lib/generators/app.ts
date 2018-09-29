8711be1e529027cede23355f1b6a76856559eff3
scott-wyatt <scottwyatt86@gmail.com>
2018-09-29 18:21:43 -0400

    [fix] test

--- lib/generators/app.ts
+++ lib/generators/app.ts
@@ -46,7 +46,7 @@ export const app = (name, answers) => {
     })
     .then(_npm => {
       console.log(colors.grey('\n------------------------------------------------\n'))
-      console.log('\nGenerated!)
+      console.log('\nGenerated!')
       console.log('\nRun ', colors.bgWhite.black(` cd ${name} && npm run compile && node dist/server `))
     })
 }
